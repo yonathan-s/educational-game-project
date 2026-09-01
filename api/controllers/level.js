0064fcd13ad9082e662db91ec48cdb7712eb8b02
Rmorbey <russellmorbey@hotmail.co.uk>
2026-09-01 12:23:11 +0100

    Created level controller. Add: index get all function, showLevel get specific level function and showStages get all stages for a specific level function.

--- api/controllers/level.js
+++ api/controllers/level.js
@@ -1,4 +1,5 @@
 const Level = require('../models/Level')
+const Stage = require('../models/Stage')
 
 async function index (req, res) {
 	try {
@@ -9,4 +10,23 @@ async function index (req, res) {
 	}
 }
 
-module.exports = { index }
+async function showLevel (req, res) {
+    try {
+        const id = parseInt(req.params.id)
+        const level = await Level.getOneById(id)
+        res.status(200).json(level)
+    } catch (err) {
+        res.status(404).json({"error": err.message})
+    }
+}
+
+async function showStages (req, res) {
+	try {
+		const level_id = parseInt(req.params.id)
+		const stages = await Stage.getByLevelId(level_id)
+		res.status(200).json(stages)
+	} catch (err) {
+		res.status(404).json({"error": err.message})
+	}
+}
+module.exports = { index, showLevel, showStages}
