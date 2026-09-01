8e87f28630e076b6bdffac0a4dee90bd3f611984
Zainab Ashter <zashter96@outlook.com>
2026-09-01 15:42:34 +0100

    updated database setup and Henry VIII seed data

--- api/database/setup.js
+++ api/database/setup.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const db = require('./connect')
 
-const sql = fs.readFileSync('./database/setup.sql').toString()
+const setupSql = fs.readFileSync('./database/setup.sql').toString()
 const seedSql = fs.readFileSync('./database/seed.sql').toString()
 
 db.query(setupSql)
@@ -14,4 +14,7 @@ db.query(setupSql)
         db.end()
         console.log("Set-up and seed complete.")
     })
-    .catch(error => console.log(error))
+    .catch(error => {
+        console.log(error)
+        db.end()
+    })
