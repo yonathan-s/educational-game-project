63e60ec4bd180af495dbe0aca73a4be7af0f7739
Rmorbey <russellmorbey@hotmail.co.uk>
2026-09-01 11:46:53 +0100

    Add: getByLevelId fetches all stages linked to a specific level.

--- api/models/Stage.js
+++ api/models/Stage.js
@@ -15,4 +15,13 @@ class Stage {
 		}
 		return new Stage(response.rows[0])
 	}
+
+	static async getByLevelId(level_id) {
+		const response = await db.query("SELECT * FROM stages WHERE level_id = $1 ORDER BY stage_number ASC", [level_id])
+		if (response.rows.length === 0) {
+			throw new Error("Unable to locate stages for this specific level.")
+		}
+
+		return response.rows.map(s => new Stage(s))
+	}
 }
