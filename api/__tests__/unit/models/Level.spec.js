683f4ec5b45bdbbb498f57b2513dcb290723f2c1
Rmorbey <russellmorbey@hotmail.co.uk>
2026-09-01 15:53:25 +0100

    Test: Level model getOneById function.

--- api/__tests__/unit/models/Level.spec.js
+++ api/__tests__/unit/models/Level.spec.js
@@ -8,13 +8,11 @@ describe("Level", () => {
 
   describe("getAll", () => {
     it("resolves with levels on successful db query", async () => {
-
       const mockLevels = [
         { id: 1, level_number: 1, level_name: "LVL 1 name" },
         { id: 2, level_number: 2, level_name: "LVL 2 name" },
       ];
       jest.spyOn(db, "query").mockResolvedValueOnce({ rows: mockLevels });
-
 
       const levels = await Level.getAll();
 
@@ -25,10 +23,38 @@ describe("Level", () => {
     });
 
     it("should throw an Error when no levels are found", async () => {
-
       jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
 
       await expect(Level.getAll()).rejects.toThrow("No levels available.");
+    });
+  });
+
+  describe("getOneById", () => {
+    it("resolves with level on successful db query", async () => {
+
+      const testLevel = { id: 1, level_number: 1, level_name: "LVL 1 name" };
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [testLevel] });
+
+      const result = await Level.getOneById(1);
+
+      // Assert
+      expect(result).toBeInstanceOf(Level);
+      expect(result.level_name).toBe("LVL 1 name");
+	  expect(result.level_number).toBe(1);
+      expect(result.id).toBe(1);
+      expect(db.query).toHaveBeenCalledWith(
+        "SELECT * FROM levels WHERE id = $1",
+        [1],
+      );
+    });
+
+    it("should throw an Error when a level is not found", async () => {
+
+      jest.spyOn(db, "query").mockResolvedValueOnce({ rows: [] });
+
+      await expect(Level.getOneById(999)).rejects.toThrow(
+        "Unable to locate level.",
+      );
     });
   });
 });
