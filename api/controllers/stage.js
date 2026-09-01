4369f0e68ac7c404ac4b35bc5b2e016b7a9edd03
Rmorbey <russellmorbey@hotmail.co.uk>
2026-09-01 12:47:02 +0100

    Fix: typo in response.

--- api/controllers/stage.js
+++ api/controllers/stage.js
@@ -4,7 +4,7 @@ async function show (req, res) {
 	try {
         const id = parseInt(req.params.id)
         const stage = await Stage.getOneById(id)
-        res.status(200).json(level)
+        res.status(200).json(stage)
     } catch (err) {
         res.status(404).json({"error": err.message})
     }
