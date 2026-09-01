f6dc38acf62a9845d4c290bd8f7dea9f46f45d8f
Zainab Ashter <zashter96@outlook.com>
2026-09-01 15:27:00 +0100

    connect setup.sql to seed.sql

--- api/database/setup.js
+++ api/database/setup.js
@@ -4,10 +4,14 @@ require('dotenv').config()
 const db = require('./connect')
 
 const sql = fs.readFileSync('./database/setup.sql').toString()
+const seedSql = fs.readFileSync('./database/seed.sql').toString()
 
-db.query(sql)
-    .then(data => {
+db.query(setupSql)
+    .then(() => {
+        return db.query(seedSql)
+    })
+    .then(() => {
         db.end()
-        console.log("Set-up complete.");
+        console.log("Set-up and seed complete.")
     })
     .catch(error => console.log(error))
