a60885b6b88af9b438095859ce8002de5d28f1c9
Rmorbey <russellmorbey@hotmail.co.uk>
2026-09-01 15:27:38 +0100

    Created sql tables reset file.

---
+++ api/__tests__/integration/reset.sql
@@ -0,0 +1,14 @@
+TRUNCATE levels RESTART IDENTITY;
+TRUNCATE stages RESTART IDENTITY;
+
+INSERT INTO levels (level_number, level_name) 
+VALUES
+    (1, 'Ancient History'),
+    (2, 'Medieval History');
+
+INSERT INTO stages (level_id, stage_number, stage_name, points)
+VALUES 
+    (1, 1, 'Early Civilisations', 100),
+    (1, 2, 'Ancient Egypt', 100),
+    (1, 3, 'Ancient Greece', 100),
+    (1, 4, 'Ancient Rome', 200),
